d9c1505845a098a584a9bbbb46513fc23b570ceb
The-Gilles-Limited <thegilleslimited@gmail.com>
2025-10-24 19:17:10 +0100

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Header } from './reusables/header/header';
+import { Footer } from "./reusables/footer/footer";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
