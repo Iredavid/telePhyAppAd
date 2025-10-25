bff6f3278570da623c5a4587a2c204b432fef336
ireoluwadavid44 <ireoluwadavid44@gmail.com>
2025-10-25 14:54:41 +0000

    new changes

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero/hero';
+import { ServiceGrid } from './service-grid/service-grid';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero,ServiceGrid],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
